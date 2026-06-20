e00295964d93c8358498f8f2b6ec8af2674af076
Abol-khls <abolfazldehghanpoorr@gmail.com>
2026-06-20 11:11:33 +0330

    setting changed

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('manager', 'مدیر'),
+        ('agent', 'مشاور'),
+        ('customer', 'مشتری'),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='agent'
+    )
+
+    phone = models.CharField(
+        max_length=20,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
