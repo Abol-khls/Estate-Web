a173d032ed79f557e8c2d468e447e3831c80a9f6
Abol-khls <abolfazldehghanpoorr@gmail.com>
2026-06-21 21:13:57 +0330

    Improve property admin panel

--- properties/admin.py
+++ properties/admin.py
@@ -1,5 +1,42 @@
 from django.contrib import admin
 from .models import Property, PropertyImage
 
-admin.site.register(Property)
-admin.site.register(PropertyImage)
+
+class PropertyImageInline(admin.TabularInline):
+    model = PropertyImage
+    extra = 1
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = (
+        'code',
+        'title',
+        'property_type',
+        'transaction_type',
+        'price',
+        'area',
+        'created_at'
+    )
+
+    search_fields = (
+        'code',
+        'title'
+    )
+
+    list_filter = (
+        'property_type',
+        'transaction_type'
+    )
+
+    inlines = [
+        PropertyImageInline
+    ]
+
+
+@admin.register(PropertyImage)
+class PropertyImageAdmin(admin.ModelAdmin):
+    list_display = (
+        'property',
+        'created_at'
+    )
