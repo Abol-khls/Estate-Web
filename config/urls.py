36e3f1931964288e3bd6c665fe6fccf0a3e11a69
Abol-khls <abolfazldehghanpoorr@gmail.com>
2026-06-22 16:04:45 +0330

    create api

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'api/',
+        include('properties.urls')
+    ),
 ]
 
 if settings.DEBUG:
@@ -28,3 +33,4 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+
