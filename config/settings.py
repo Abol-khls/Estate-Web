ed1c53f1c79cc59deeafb9b31222f6e2bb7928b0
Abol-khls <abolfazldehghanpoorr@gmail.com>
2026-06-22 16:13:52 +0330

    Add customer REST API

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,9 @@ AUTH_USER_MODEL = 'users.User'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
