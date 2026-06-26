e828f8ee6a07507553c0b40ea0f6f89d584a9aaf
Abol-khls <abolfazldehghanpoorr@gmail.com>
2026-06-26 20:15:01 +0330

    Add Axios interceptor for automatic JWT authorization

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,42 @@
+import { useEffect } from "react";
+import api from "../../services/api";
+
 export default function Dashboard() {
+
+    useEffect(() => {
+
+        async function loadData() {
+
+            try {
+
+                const response = await api.get(
+                    "/dashboard/"
+                );
+
+                console.log(response.data);
+
+            }
+
+            catch (error) {
+
+                console.log(error);
+
+            }
+
+        }
+
+        loadData();
+
+    }, []);
+
     return (
-        <h1>Dashboard</h1>
+
+        <h1>
+
+            Dashboard
+
+        </h1>
+
     );
+
 }
