1a0a64c30871d8584e46820b2d4dd7cd4b6db780
Abol-khls <abolfazldehghanpoorr@gmail.com>
2026-07-29 02:29:25 +0330

    chore(eslint): update project lint configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,7 +23,9 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]', args: 'after-used' }],
+      'eqeqeq': ['error', 'smart'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
     },
   },
 ])
