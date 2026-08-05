aa15eea235a43c666600f353ca93ee312871ed87
Abol-khls <abolfazldehghanpoorr@gmail.com>
2026-08-06 02:44:33 +0330

    feat(auth): add cookie-based authentication endpoints

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,16 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.exceptions import ValidationError
 from drf_spectacular.utils import extend_schema, OpenApiResponse
 from drf_spectacular.types import OpenApiTypes
+
+from django.conf import settings as django_settings
 
 from core.permissions import IsManager
 from core.viewsets import AgencyScopedViewSet
 from core.mixins import AuditActorMixin
 from core.throttling import LoginIPRateThrottle, LoginUsernameRateThrottle
 
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+    TokenRefreshSerializer,
+)
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 
 from .serializers import (
     UserSerializer,
@@ -18,6 +25,26 @@ from .serializers import (
     TeamMemberSerializer,
 )
 from .models import User
+
+
+REFRESH_COOKIE_NAME = 'refresh_token'
+
+REFRESH_COOKIE_PATH = '/api/token/'
+
+
+def set_refresh_cookie(response, refresh_token):
+
+    response.set_cookie(
+        REFRESH_COOKIE_NAME,
+        str(refresh_token),
+        max_age=int(
+            django_settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'].total_seconds()
+        ),
+        httponly=True,
+        secure=not django_settings.DEBUG,
+        samesite='Lax',
+        path=REFRESH_COOKIE_PATH,
+    )
 
 
 class MeView(AuditActorMixin, APIView):
@@ -178,9 +205,85 @@ class TeamViewSet(AgencyScopedViewSet):
         instance.delete()
 
 
-class ThrottledTokenObtainPairView(TokenObtainPairView):
+class CookieTokenObtainPairView(APIView):
+
+    permission_classes = [AllowAny]
 
     throttle_classes = [
         LoginIPRateThrottle,
         LoginUsernameRateThrottle,
     ]
+
+    @extend_schema(responses={200: OpenApiTypes.OBJECT})
+    def post(self, request):
+
+        serializer = TokenObtainPairSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        data = dict(serializer.validated_data)
+
+        refresh = data.pop('refresh')
+
+        response = Response(data)
+
+        set_refresh_cookie(response, refresh)
+
+        return response
+
+
+class CookieTokenRefreshView(APIView):
+
+    permission_classes = [AllowAny]
+
+    @extend_schema(responses={200: OpenApiTypes.OBJECT})
+    def post(self, request):
+
+        raw_refresh = request.COOKIES.get(REFRESH_COOKIE_NAME)
+
+        if not raw_refresh:
+            raise InvalidToken('No refresh token cookie found.')
+
+        serializer = TokenRefreshSerializer(data={'refresh': raw_refresh})
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as error:
+            raise InvalidToken(str(error))
+
+        data = dict(serializer.validated_data)
+
+        new_refresh = data.pop('refresh', None)
+
+        response = Response(data)
+
+        if new_refresh:
+            set_refresh_cookie(response, new_refresh)
+
+        return response
+
+
+class CookieTokenLogoutView(APIView):
+
+    permission_classes = [AllowAny]
+
+    @extend_schema(responses={200: OpenApiTypes.OBJECT})
+    def post(self, request):
+
+        raw_refresh = request.COOKIES.get(REFRESH_COOKIE_NAME)
+
+        if raw_refresh:
+
+            try:
+                RefreshToken(raw_refresh).blacklist()
+            except TokenError:
+                pass
+
+        response = Response({"detail": "خارج شدید."})
+
+        response.delete_cookie(
+            REFRESH_COOKIE_NAME,
+            path=REFRESH_COOKIE_PATH,
+        )
+
+        return response
