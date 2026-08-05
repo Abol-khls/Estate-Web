f946d1d4dd39abc09ecf7ee5d17a91189925129c
Abol-khls <abolfazldehghanpoorr@gmail.com>
2026-08-06 02:45:05 +0330

    chore(config): enable CORS credentials for cookie authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,6 +25,8 @@ CORS_ALLOWED_ORIGINS = config(
     "CORS_ALLOWED_ORIGINS"
 ).split(",")
 
+CORS_ALLOW_CREDENTIALS = True
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
